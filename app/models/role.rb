30fa657d5845cac00913ddc65045eab691b3a6db
dgriego <daniel@jackrabbitsystems.com>
2017-05-03 18:08:32 -0700

    add uniqueness to name

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,5 @@ class Role < ApplicationRecord
   has_many :role_users
   has_many :users, through: :role_users
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User, ChildTrip, Child, RoleUser, Role, Trip
+# consider another way to create an admin user
+# Create Users
+# Create Roles for Users
+# Create Kids
+# Create Trips
+# Add Kids to a Trip
+# probably add the kids and create the trips last thing
